23331f06a01e71cd520de0d84de4a6a931468ba2
ranjitkc7 <ranjit252@gmail.com>
2025-09-25 12:38:10 +0545

    first-commit

---
+++ dart.dart
@@ -0,0 +1,45 @@
+// void main() {
+//  print("Hello world, I am Ranjit K.C");
+//  print("I want to learn flutter");
+// }
+
+// String name = "Ranjit K.C";
+// int age = 21;
+// String address = "Syangja";
+
+// void greet(String n, int a, String add) {
+//   print("Name = $n, Age = $a, Address = $add");
+// }
+
+// void main() {
+//   greet(name, age, address);
+// }
+// void main() {
+//   var name = "Ranjit Kshetry";
+//   var age = 22;
+//   var height = 5.4;
+//   var hobbies = ['volleyball', 'dancing', 'coding', 'singing'];
+
+//   var images = {'tags': "ranjitkc", 'url': '//path/to/ranjit.jpg'};
+
+//   print("Pesonal Info:");
+//   print("Name = $name");
+//   print("Age = $age");
+//   print("Height = $height");
+//   if(age >= 18){
+//     print("\n$name is eligible fot the voting.");
+//   }else if(age >= 16 && age < 18){
+//     print("\n$name is able to get voter card but not able to vote.");
+//   }else{
+//     print("\n$name is not eligible for getting voter card and do vote.");
+
+//   print("\nHis Hobblies:");
+//   for (var hobby in hobbies) {
+//     print(" - $hobby");
+//   }
+//   ;
+
+//   print("\nImage Info:");
+//   print("Tags: ${images['tags']}");
+//   print("URL : ${images['url']}");
+// }
