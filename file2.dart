e0048e2e6cb7bd3bb6750d78898c6e6f3132d506
ranjitkc7 <ranjit252@gmail.com>
2025-09-25 22:06:23 +0545

    third-commit

---
+++ file2.dart
@@ -0,0 +1,34 @@
+import 'dart:math';
+import 'utils.dart';
+
+class Person {
+  String name;
+  int age;
+
+  Person(this.name, this.age);
+
+  void introduction() {
+    print("My name is $name");
+    print("I am $age years old");
+  }
+}
+
+void main() {
+  // var randomNumber = Random().nextInt(100);
+  // print("The generated random number is : $randomNumber");
+
+  // var squareRootNumber = sqrt(144);
+  // print("The square root of  is : $squareRootNumber");
+
+  // var power = pow(2, 5);
+  // print("The power is : $power");
+
+  // greet("Ranjit", 22);
+
+  var person1 = Person("Sameer KC", 12);
+  var person2 = Person("Harthik KC", 14);
+
+  person1.introduction();
+  person2.introduction();
+}
+
---
+++ utils.dart
@@ -0,0 +1,4 @@
+void greet(String name, int age) {
+  print("Hello $name");
+  print("You are $age years old");
+}
